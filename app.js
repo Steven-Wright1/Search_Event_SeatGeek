451e1b2db3daa16619e4a239c750c9877e905ed5
Steven Wright <71319046+Steven-Wright1@users.noreply.github.com>
2022-12-20 19:19:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,31 @@ function search(e) {
   resultsEl.innerHTML = `<p> ${performing} 
   <br/> current events:${data.performers[0].num_upcoming_events}.</p><br/>`;
   console.log(data.performers[0].num_upcoming_events)
+    
+    
+     //changes below committed by Steven
+    let performer_id = data.performers[0].id;
+    let apirequest2 = `https://api.seatgeek.com/2/events?performers.id=${performer_id}&client_id=${apiKey}`;
+  
+    fetch(apirequest2).then(response=>{
+        return response.json();
+    })
+    .then((data) => {
+      
+      let event_counter = data.events.length
+
+      for (let i = 0; i < event_counter; i++){
+        let event = data.events[i];
+        console.log(event.title)
+        console.log(event.datetime_local)
+        console.log(event.type)
+        console.log(event.venue.display_location)
+      }
+
+        console.log(data)
+    });
+    
+    
 })
 };
 
